25698ca0c13cd8ed9276c7e61629226e54510bb8
dorongjik <dorongjik@gmail.com>
2017-08-10 10:47:16 +0900

    add room controller : get room_id from url and return it by text

--- crypto_blog/crypto_blog/urls.py
+++ crypto_blog/crypto_blog/urls.py
@@ -6,8 +6,12 @@ from django.http.response import HttpResponse
 def home(request):
     return HttpResponse("Hello world")
 
+def room(request, room_id):
+    return HttpResponse("room number : " + room_id)
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
     url(r'^$', home), # 여기에 있는 home은 함수 home 임.
+    url(r'^rooms/(?P<room_id>\d+)/$', room),
 ]
