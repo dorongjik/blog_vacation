36a7fd1dc883ddc65684e52f749cb01fce7fdd7b
dorongjik <dorongjik@gmail.com>
2017-08-21 14:33:49 +0900

    seperate views & templates

--- crypto_blog/crypto_blog/views.py
+++ crypto_blog/crypto_blog/views.py
@@ -1,8 +1,17 @@
 from django.http.response import HttpResponse
+from django.conf import settings
+from django.template import loader
+
 import requests
 
 def home(request):
-    return HttpResponse("Hello world")
+    template = loader.get_template("home.html")
+    return HttpResponse(
+        template.render(
+            {"site_name" : "대박 투자"},
+            request,
+        )
+    )
 
 def room(request, room_id):
     url = "https://api.zigbang.com/v3/items?detail=true&item_ids=[" + room_id + "]"
