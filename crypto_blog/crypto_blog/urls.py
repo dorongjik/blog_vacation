e0c24655df195617b035c8a228d56e4b4e7f7f9e
dorongjik <dorongjik@gmail.com>
2017-08-21 18:43:54 +0900

    refactor urls.py : add name

--- crypto_blog/crypto_blog/urls.py
+++ crypto_blog/crypto_blog/urls.py
@@ -6,6 +6,6 @@ from crypto_blog.views import home, room # 함수들을 불러옴.
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^$', home), # 여기에 있는 home은 함수 home 임.
-    url(r'^rooms/(?P<room_id>\d+)/$', room),
+    url(r'^$', home, name="home"), # 여기에 있는 home은 함수 home 임.
+    url(r'^rooms/(?P<room_id>\d+)/$', room, name="room"),
 ]
