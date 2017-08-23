f6ac05389ccaefc3cbff6bb43c457be81044dbad
dorongjik <dorongjik@gmail.com>
2017-08-23 14:57:00 +0900

    update link : policies in footer

--- crypto_blog/crypto_blog/urls.py
+++ crypto_blog/crypto_blog/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
 
     url(r'^$', home, name="home"), # 앞의 home은 함수 home, 뒤의 name=home은 template쪽에서 django template("{% url : home %}")을 쓰기 위한 것.
     url(r'^rooms/(?P<room_id>\d+)/$', room, name="room"),
+    url(r'^about/$', about, name="about"),
+
+    url(r'^policy/terms/$', terms, name="terms"),
+    url(r'^policy/privacy/$', privacy, name="privacy"),
+    url(r'^policy/disclaimer/$', disclaimer, name="disclaimer"),
 ]
--- crypto_blog/crypto_blog/views/__init__.py
+++ crypto_blog/crypto_blog/views/__init__.py
@@ -1,2 +1,5 @@
 from .home import home
 from .room import room
+from .about import about
+
+from .policy import *
