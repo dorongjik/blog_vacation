49060580c9a0db8f4682562110502f989265e366
dorongjik <dorongjik@gmail.com>
2017-08-23 11:31:53 +0900

    delete controller.py and views.py : because of refactoring

--- crypto_blog/crypto_blog/controller.py
+++
@@ -1,7 +0,0 @@
-from django.http.response import HttpResponse
-
-def home(request):
-    return HttpResponse("Hello world")
-
-def room(request, room_id):
-    return HttpResponse("room number : " + room_id)
